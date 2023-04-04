bc15e31f88a2dcf0ba96fbeff6a75b299d6ffeeb
anshulkhare-iitd <anshulkhare.iitd@gmail.com>
2023-04-04 13:34:09 +0530

    chore: created react and express apps

---
+++ apps/suki-express/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'suki-express',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/suki-express',
+};
